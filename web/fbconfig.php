05e59e7ae9e15113a412a3c2177cc075d2ae2a65
raksa <eng.raksa@gmail.com>
2016-09-19 22:21:51 +0700

    update

--- web/fbconfig.php
+++ web/fbconfig.php
@@ -16,40 +16,62 @@ use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
 // init app with app id and secret
+// init app with app id and secret
 FacebookSession::setDefaultApplication( '307509092945291','3f12b5853b84ac765c9d56ecf2e95c43' );
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('https://up-excel.herokuapp.com/fbconfig.php' );
-try {
-  $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
-  // When Facebook returns an error
-} catch( Exception $ex ) {
-  // When validation fails or other local issues
+$helper = new FacebookRedirectLoginHelper( 'https://up-excel.herokuapp.com/index.php' );
+// see if a existing session exists
+if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+  // create new session from saved access_token
+  $session = new FacebookSession( $_SESSION['fb_token'] );
+  
+  // validate the access_token to make sure it's still valid
+  try {
+    if ( !$session->validate() ) {
+      $session = null;
+    }
+  } catch ( Exception $e ) {
+    // catch any exceptions
+    $session = null;
+  }
+}  
+if ( !isset( $session ) || $session === null ) {
+  // no session exists
+  
+  try {
+    $session = $helper->getSessionFromRedirect();
+  } catch( FacebookRequestException $ex ) {
+    // When Facebook returns an error
+    // handle this better in production code
+    print_r( $ex );
+  } catch( Exception $ex ) {
+    // When validation fails or other local issues
+    // handle this better in production code
+    print_r( $ex );
+  }
+  
 }
-
-$session = new FacebookSession('ce487547326fe035108288bbf266a51d');
-$request = new FacebookRequest($session, 'GET', '/me');
-$response = $request->execute();
-var_dump($response);
-
 // see if we have a session
 if ( isset( $session ) ) {
+  
+  // save the session
+  $_SESSION['fb_token'] = $session->getToken();
+  // create a session using saved token or the new one we generated at login
+  $session = new FacebookSession( $session->getToken() );
+  
   // graph api request for user data
   $request = new FacebookRequest( $session, 'GET', '/me' );
   $response = $request->execute();
   // get response
-  $graphObject = $response->getGraphObject();
-     	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
- 	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
-	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
-	/* ---- Session Variables -----*/
-	    $_SESSION['FBID'] = $fbid;           
-        $_SESSION['FULLNAME'] = $fbfullname;
-	    $_SESSION['EMAIL'] =  $femail;
-    /* ---- header location after session ----*/
-  // header("Location: index.php");
+  $graphObject = $response->getGraphObject()->asArray();
+  
+  // print profile data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+  
+  // print logout url using session and redirect_uri (logout.php page should destroy the session)
+  echo '<a href="' . $helper->getLogoutUrl( $session, 'https://up-excel.herokuapp.com/logout.php' ) . '">Logout</a>';
+  
 } else {
- //  $loginUrl = $helper->getLoginUrl();
- // header("Location: ".$loginUrl);
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
 }
-?>
--- web/index.php
+++ web/index.php
@@ -1,38 +1,77 @@
 <?php
-session_start(); 
-?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Login with Facebook</title>
-<link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
- </head>
-  <body>
-  <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
-<div class="container">
-<div class="hero-unit">
-  <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
-  <p>Welcome to "facebook login" tutorial</p>
-  </div>
-<div class="span4">
- <ul class="nav nav-list">
-<li class="nav-header">Image</li>
-	<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
-<li class="nav-header">Facebook ID</li>
-<li><?php echo  $_SESSION['FBID']; ?></li>
-<li class="nav-header">Facebook fullname</li>
-<li><?php echo $_SESSION['FULLNAME']; ?></li>
-<li class="nav-header">Facebook Email</li>
-<li><?php echo $_SESSION['EMAIL']; ?></li>
-<div><a href="logout.php">Logout</a></div>
-</ul></div></div>
-    <?php else: ?>     <!-- Before login --> 
-<div class="container">
-<h1>Login with Facebook</h1>
-           Not Connected
-<div>
-      <a href="fbconfig.php">Login with Facebook</a></div>
-      </div>
-    <?php endif ?>
-  </body>
-</html>
+echo "start test"
+
+session_start();
+// added in v4.0.0
+require_once 'autoload.php';
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+// init app with app id and secret
+// init app with app id and secret
+FacebookSession::setDefaultApplication( '307509092945291','3f12b5853b84ac765c9d56ecf2e95c43' );
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'https://up-excel.herokuapp.com/index.php' );
+// see if a existing session exists
+if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+  // create new session from saved access_token
+  $session = new FacebookSession( $_SESSION['fb_token'] );
+  
+  // validate the access_token to make sure it's still valid
+  try {
+    if ( !$session->validate() ) {
+      $session = null;
+    }
+  } catch ( Exception $e ) {
+    // catch any exceptions
+    $session = null;
+  }
+}  
+if ( !isset( $session ) || $session === null ) {
+  // no session exists
+  
+  try {
+    $session = $helper->getSessionFromRedirect();
+  } catch( FacebookRequestException $ex ) {
+    // When Facebook returns an error
+    // handle this better in production code
+    print_r( $ex );
+  } catch( Exception $ex ) {
+    // When validation fails or other local issues
+    // handle this better in production code
+    print_r( $ex );
+  }
+  
+}
+// see if we have a session
+if ( isset( $session ) ) {
+  
+  // save the session
+  $_SESSION['fb_token'] = $session->getToken();
+  // create a session using saved token or the new one we generated at login
+  $session = new FacebookSession( $session->getToken() );
+  
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject()->asArray();
+  
+  // print profile data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+  
+  // print logout url using session and redirect_uri (logout.php page should destroy the session)
+  echo '<a href="' . $helper->getLogoutUrl( $session, 'https://up-excel.herokuapp.com/logout.php' ) . '">Logout</a>';
+  
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
+}
