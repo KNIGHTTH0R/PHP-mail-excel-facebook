da0a3d37b27f9cb15adab40f2e0758cd789fa5cb
raksa <eng.raksa@gmail.com>
2016-09-20 17:23:20 +0700

    update

--- web/send_mail.php
+++ web/send_mail.php
@@ -23,24 +23,23 @@ $mail->AltBody = 'name:Eng Raksa\n id:123';*/
 require '../vendor/autoload.php';
 
 $mail = new PHPMailer(true);
-$mail->IsSMTP(); // telling the class to use SMTP
+$mail->IsSMTP();
 try {
-//    $mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
-    $mail->SMTPAuth = true;                  // enable SMTP authentication
-    $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
-    $mail->Host = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-    $mail->Port = 587;                   // set the SMTP port for the GMAIL server
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
     $mail->Username = $email;
     $mail->Password = $password;
     $mail->AddAddress('eng.raksa@gmail.com');
     $mail->Subject = 'PHPMailer Test Subject via mail(), advanced';
-    $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
+    $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!';
     $mail->MsgHTML(file_get_contents('contents.html'));
     $mail->Body = 'This is body';
     $mail->Send();
     echo "Message Sent OK<p></p>\n";
 } catch (phpmailerException $e) {
-    echo $e->errorMessage(); //Pretty error messages from PHPMailer
+    echo $e->errorMessage();
 } catch (Exception $e) {
-    echo $e->getMessage(); //Boring error messages from anything else!
+    echo $e->getMessage();
 }
