4430f9d9325f290363aa895fbe2ec8aa84503f12
raksa <eng.raksa@gmail.com>
2016-09-20 16:04:27 +0700

    update

--- web/file_upload.php
+++ web/file_upload.php
@@ -66,7 +66,7 @@ if (isset($_FILES[$file_name]) && isset($_POST['email'])) {
         $name = $info['filename'];
         $i = 0;
         $suf = '';
-        $dist = basename(__DIR__) . 'files/';
+        $dist = basename(__DIR__) . '/' . 'files/';
         error_log($dist);
         do {
             $newFileName = $name . $suf . '.' . $ext;
