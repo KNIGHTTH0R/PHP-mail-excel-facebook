e57f4ea5b420ad37e1a019e8016d472df2d1815b
raksa <eng.raksa@gmail.com>
2016-09-20 11:24:02 +0700

    update

--- web/index.php
+++ web/index.php
@@ -1,4 +1,32 @@
 <?php
-echo "hello world";
-?>
-<a href="/web/login.php">Login</a>
+require_once '/vendor/autoload.php';
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '307509092945291',
+  'app_secret' => '3f12b5853b84ac765c9d56ecf2e95c43',
+  'default_graph_version' => 'v2.7',
+  //'default_access_token' => '{access-token}', // optional
+]);
+
+// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+//   $helper = $fb->getRedirectLoginHelper();
+//   $helper = $fb->getJavaScriptHelper();
+//   $helper = $fb->getCanvasHelper();
+//   $helper = $fb->getPageTabHelper();
+
+try {
+  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+  // If you provided a 'default_access_token', the '{access-token}' is optional.
+  $response = $fb->get('/me', 'ce487547326fe035108288bbf266a51d');
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$me = $response->getGraphUser();
+echo 'Logged in as ' . $me->getName();
