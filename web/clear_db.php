0d2b162007b181a7a150241c88aec923bce9c930
raksa <eng.raksa@gmail.com>
2016-09-20 18:01:33 +0700

    update

--- web/clear_db.php
+++ web/clear_db.php
@@ -3,8 +3,18 @@ require_once 'db.php';
 $db = connect();
 if ($db) {
     clearAll($db);
-    unlink(getcwd() . '/../' . 'files/*.xls');
-    unlink(getcwd() . '/../' . 'files/*.xlsx');
+    function deleteForExt($ext)
+    {
+        foreach (glob(getcwd() . '/../' . 'files/*.' . $ext) as $filename) {
+            if (is_file($filename)) {
+                unlink($filename);
+            }
+        }
+    }
+
+    deleteForExt('xls');
+    deleteForExt('xlsx');
+
     $db->close();
 } else {
     $_SESSION[$SESSION_DB_MESSAGE] = 'Can\'t connect to database.';
