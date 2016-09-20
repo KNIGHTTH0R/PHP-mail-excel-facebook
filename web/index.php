7a1d920d2d2cc903fd1b9d8861f723f5760467d1
raksa <eng.raksa@gmail.com>
2016-09-20 11:29:45 +0700

    update

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ $fb = new \Facebook\Facebook([
   'app_id' => '307509092945291',
   'app_secret' => '3f12b5853b84ac765c9d56ecf2e95c43',
   'default_graph_version' => 'v2.7',
-  //'default_access_token' => '{access-token}', // optional
+  'default_access_token' => 'ce487547326fe035108288bbf266a51d', // optional
 ]);
 
 // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
@@ -17,7 +17,7 @@ $fb = new \Facebook\Facebook([
 try {
   // Get the \Facebook\GraphNodes\GraphUser object for the current user.
   // If you provided a 'default_access_token', the '{access-token}' is optional.
-  $response = $fb->get('/me', 'ce487547326fe035108288bbf266a51d');
+  $response = $fb->get('/me');
 } catch(\Facebook\Exceptions\FacebookResponseException $e) {
   // When Graph returns an error
   echo 'Graph returned an error: ' . $e->getMessage();
